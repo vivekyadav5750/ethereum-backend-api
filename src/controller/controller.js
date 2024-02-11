fdeced1321c91b7949dab36fcea784012cd979c3
Vivek <10Vivekkumaryadav@gmail.com>
2024-02-11 13:05:50 +0530

    change value to eth and balance in eth and show symbol eth, usd

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -4,6 +4,7 @@ const config = require('../../config/config');
 
 // Your Etherscan API Key
 const apiKey = config.apiKey;
+
 exports.getAddressBalance = async (req, res) => {
     try{
         const address = req.params.address;
@@ -18,15 +19,15 @@ exports.getAddressBalance = async (req, res) => {
         // const ethPriceInUsd = parseFloat(priceResponse.data.result.ethusd);
 
         // OR 
-        const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
-        const ethPriceInUsd = response.data.ethereum.usd;
+        // const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
+        // const ethPriceInUsd = response.data.ethereum.usd;
 
-        // 3. Calculate balance in USD
-        const balanceInUsd = balanceInEth * ethPriceInUsd;
+        // // 3. Calculate balance in USD
+        // const balanceInUsd = balanceInEth * ethPriceInUsd;
 
-        // 4. balance in 2 decimal
-        const balanceInUsd2Decimal = balanceInUsd.toFixed(2);
-
+        // // 4. balance in 2 decimal
+        // const balanceInUsd2Decimal = balanceInUsd.toFixed(2) + " USD";
+        const balanceInUsd2Decimal = balanceInEth.toFixed(5) + " ETH";
         return res.json({ success: true, address, balance:balanceInUsd2Decimal });
 
     }
@@ -48,24 +49,33 @@ exports.getTransactionDeatils = async (req, res) => {
 
         // 2.Iterate over each transaction object and extract required fields
         transactions.forEach(transaction => {
-        const { hash, from, to, value, timeStamp } = transaction;
-        time = new Date(timeStamp * 1000);
-        old_time = new Date(timeStamp * 1000);
-        new_time = new Date()
-        const differenceInMilliseconds = Math.abs(new_time.getTime() - old_time.getTime());
+            const { hash, from, to, value, timeStamp } = transaction;
+            var eth = value / 10**18; // Convert from Wei to Ether
+            eth = parseFloat(eth.toFixed(5));
+            if (eth <= 0)
+            {
+                eth = "1 wei"
+            }
+            else{
+                eth = eth + " eth"
+            }
+            //calculate time age
+            time = new Date(timeStamp * 1000);
+            old_time = new Date(timeStamp * 1000);
+            new_time = new Date()
+            const differenceInMilliseconds = Math.abs(new_time.getTime() - old_time.getTime());
 
-        // 3.Convert milliseconds to days and hours
-        const millisecondsInDay = 1000 * 60 * 60 * 24;
-        const days = Math.floor(differenceInMilliseconds / millisecondsInDay);
-        const hours = Math.floor((differenceInMilliseconds % millisecondsInDay) / (1000 * 60 * 60));
-
-        // 4.Output the result
-        time = `${days} days  ${hours} hours`;
-        extractedData.push({ hash, from, to, value, time });
+            // 3.Convert milliseconds to days and hours
+            const millisecondsInDay = 1000 * 60 * 60 * 24;
+            const days = Math.floor(differenceInMilliseconds / millisecondsInDay);
+            const hours = Math.floor((differenceInMilliseconds % millisecondsInDay) / (1000 * 60 * 60));
+            time = `${days} days  ${hours} hours`;
+            
+            // 4.Output the result
+            extractedData.push({ hash, from, to, eth, time });
         });
 
         return res.json({ success: true, address, transactions:extractedData });
-
     }
     catch(error){
         console.error('Error fetching ETH Transaction:', error.message);
@@ -77,7 +87,7 @@ exports.getTransactionDeatils = async (req, res) => {
 exports.getEthereumPrice = async (req, res) => {
     try {
         const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
-        const ethereumPrice = response.data.ethereum.usd;
+        const ethereumPrice = response.data.ethereum.usd + " USD";
         res.json({ success: true, ethereumPrice });
     } catch (error) {
         console.error(error);
