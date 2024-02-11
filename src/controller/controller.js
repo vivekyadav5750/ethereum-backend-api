8a495a3e3479068afcaefc93a39480dd007d2f53
Vivek <10Vivekkumaryadav@gmail.com>
2024-02-11 07:21:39 +0530

    address api seperate into 2 api transaction and balance

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,18 +1,17 @@
 const axios = require('axios');
 const Address = require('../models/model'); // Add the missing import for the Address model
+const config = require('../../config/config');
 
 // Your Etherscan API Key
-const apiKey = 'RQQAGD6QAGT7399EHRYPJRZUYKVZRUSGEG';
-exports.getAddressDetails = async (req, res) => {
+const apiKey = config.apiKey;
+exports.getAddressBalance = async (req, res) => {
     try{
         const address = req.params.address;
-        console.log("Address : ",address);
 
         // 1. Balance of Ethereum address
         const balanceResponse = await axios.get(`https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=${apiKey}`);
         const balanceInWei = parseInt(balanceResponse.data.result);
         const balanceInEth = balanceInWei / 10**18;
-        console.log("balance : ",balanceInEth);
 
         // 2. Get current ETH price in USD
         // const priceResponse = await axios.get(`https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${apiKey}`);
@@ -21,25 +20,33 @@ exports.getAddressDetails = async (req, res) => {
         // OR 
         const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
         const ethPriceInUsd = response.data.ethereum.usd;
-        console.log("Price in usd : ",ethPriceInUsd);
 
         // 3. Calculate balance in USD
         const balanceInUsd = balanceInEth * ethPriceInUsd;
-        console.log("balance in usd : ",balanceInUsd);
 
         // 4. balance in 2 decimal
         const balanceInUsd2Decimal = balanceInUsd.toFixed(2);
-        console.log("balance in usd 2 decimal : ",balanceInUsd2Decimal);
 
-<<<<<<< HEAD
-        
-        // 5. Fetch Ethereum transactions of given address
+        return res.json({ success: true, address, balance:balanceInUsd2Decimal });
+
+    }
+    catch(error){
+        console.error('Error fetching ETH balance:', error.message);
+        res.status(500).json({ success: false, message: 'Error fetching address balance' });
+    }
+}
+
+exports.getTransactionDeatils = async (req, res) => {
+    try{
+        const address = req.params.address;
+    
+        // 1. Fetch Ethereum transactions of given address
         const transactionsResponse = await axios.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${apiKey}`);
         const transactions = transactionsResponse.data.result;
 
         const extractedData = [];
 
-        // Iterate over each transaction object and extract required fields
+        // 2.Iterate over each transaction object and extract required fields
         transactions.forEach(transaction => {
         const { hash, from, to, value, timeStamp } = transaction;
         time = new Date(timeStamp * 1000);
@@ -47,36 +54,25 @@ exports.getAddressDetails = async (req, res) => {
         new_time = new Date()
         const differenceInMilliseconds = Math.abs(new_time.getTime() - old_time.getTime());
 
-        // Convert milliseconds to days and hours
+        // 3.Convert milliseconds to days and hours
         const millisecondsInDay = 1000 * 60 * 60 * 24;
         const days = Math.floor(differenceInMilliseconds / millisecondsInDay);
         const hours = Math.floor((differenceInMilliseconds % millisecondsInDay) / (1000 * 60 * 60));
 
-        // Output the result
-        console.log(`${days} days and ${hours} hours`);
+        // 4.Output the result
         time = `${days} days  ${hours} hours`;
         extractedData.push({ hash, from, to, value, time });
         });
 
-        // console.log(extractedData);
-
-        return res.json({ success: true, address, balance:balanceInUsd2Decimal, transactions:extractedData });
-=======
-        // 5. Fetch Ethereum transactions of given address
-        const transactionsResponse = await axios.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${apiKey}`);
-        const transactions = transactionsResponse.data.result;
-        console.log("Transactions : ",transactions);
-
-        // res.json({ success: true, address, balanceInEth });
-        return res.json({ success: true, address, balanceInUsd2Decimal, transactions });
->>>>>>> c032bcabb4d25d1952f2ddfc66f64d798b65f552
+        return res.json({ success: true, address, transactions:extractedData });
 
     }
     catch(error){
-        console.error('Error fetching ETH balance:', error.message);
-        res.status(500).json({ success: false, message: 'Error fetching address details' });
+        console.error('Error fetching ETH Transaction:', error.message);
+        res.status(500).json({ success: false, message: 'Error fetching address Transaction' });
     }
 }
+
 
 exports.getEthereumPrice = async (req, res) => {
     try {
--- src/controller/routes.js
+++ src/controller/routes.js
@@ -4,6 +4,7 @@ const controller = require('./controller');
 const router = express.Router();
 
 router.get('/ethereum/price', controller.getEthereumPrice);
-router.get('/ethereum/transactions/:address', controller.getAddressDetails);
+router.get('/ethereum/balance/:address', controller.getAddressBalance);
+router.get('/ethereum/transactions/:address', controller.getTransactionDeatils);
 
 module.exports = router;
